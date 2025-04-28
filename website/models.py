1dfef683e3141285e1cacf151d963ab4e0d99c45
Andrew Sliva <148899989+ajs2583@users.noreply.github.com>
2025-04-27 22:36:51 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,5 +1,14 @@
 from . import db
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    departure_date = db.Column(db.Date, nullable=False)
+    price = db.Column(db.Float, nullable=True)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
